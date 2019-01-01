8e8cc22df108acdbbf8539c40d2fe49ec3aaea4f
OutisJie <1014547032@qq.com>
2019-01-02 00:11:29 +0800

    add nav

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '远书',
+  description: '武杰的博客',
+  head: [
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/'},
+      { text: '学习', link: '/learn'},
+      { text: '手记', link: '/handnote'},
+      { text: '关于' , link: '/aboutme'},
+      { text: 'GitHub', link: 'https://github.com/OutisJie/farbook'}
+    ]
+  }
+}
