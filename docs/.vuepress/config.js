10583999d32c9f6581db7d5961a579727e0d1572
OutisJie <1014547032@qq.com>
2019-01-25 19:39:33 +0800

    update: 更新-aboutme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
 module.exports = {
   title: '远书',
   description: '武杰的博客',
-  base: '/farbook/', 
+  base: '/farbook/',
   repo: 'https://github.com/OutisJie/farbook',
   head: [
     ['link', { rel: 'icon', href: '/img/favicon.ico' }]
   ],
   themeConfig: {
     nav: [
-      { text: '主页', link: '/'},
-      { text: '学习', link: '/learn/'},
-      { text: '手记', link: '/handnote/'},
-      { text: '关于' , link: '/about/'},
-      { text: 'GitHub', link: 'https://github.com/OutisJie/farbook'}
+      { text: '主页', link: '/' },
+      { text: '学习', link: '/learn/' },
+      { text: '手记', link: '/handnote/' },
+      { text: '关于', link: '/about/' },
+      { text: 'GitHub', link: 'https://github.com/OutisJie/farbook' }
     ],
     sidebar: {
       '/learn/': [
@@ -22,6 +22,10 @@ module.exports = {
       '/handnote/': [
         '',
         'hangzhou'
+      ],
+      '/about/': [
+        '',
+        'outisjie'
       ]
     }
   }
