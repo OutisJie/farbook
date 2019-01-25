94691ed4f3d1d5de4bcf018c9414c31888f111ba
OutisJie <1014547032@qq.com>
2019-01-25 11:44:55 +0800

    feat(update): 将项目发布到farbook的gh-page分支下

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: '远书',
   description: '武杰的博客',
+  base: '/farbook/', 
+  repo: 'https://github.com/OutisJie/farbook',
   head: [
     ['link', { rel: 'icon', href: '/img/favicon.ico' }]
   ],
@@ -9,7 +11,7 @@ module.exports = {
       { text: '主页', link: '/'},
       { text: '学习', link: '/learn/'},
       { text: '手记', link: '/handnote/'},
-      { text: '关于' , link: '/about.md'},
+      { text: '关于' , link: '/about/'},
       { text: 'GitHub', link: 'https://github.com/OutisJie/farbook'}
     ],
     sidebar: {
